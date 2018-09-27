f1fa8f589b7b977ec7371e2b4c6433bd2263e287
Bence Acs <acs.bence94@gmail.com>
2018-09-27 13:22:10 +0200

    AB: Added recombination on a line

--- matlab/recombine.m
+++ matlab/recombine.m
@@ -11,8 +11,16 @@ for i=1:N
         n=floor(rand*N)+1;
         p(i,2)=selected(r2(n));
     end;
+    %parent_pairs(pair_id,chromosome_id,parent_id(1 vs 2))
+    par(i,:,1)=population(p(i,1),:);
+    par(i,:,2)=population(p(i,2),:);
 end;
 
 %Recombination
-
+for i=1:N
+    r=rand*1.5-0.25;
+    a(i)=r;
+    o(i,:)=par(i,:,1)+a(i)*(par(i,:,2)-par(i,:,1));
+end;
+offspring=o;
     
