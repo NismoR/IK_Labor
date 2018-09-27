1773a85cef078d772a900b4a4e39a0ae5e243a48
Bence Acs <acs.bence94@gmail.com>
2018-09-27 12:42:05 +0200

    AB: RWS uses cumsum from now

--- matlab/rws.m
+++ matlab/rws.m
@@ -1,8 +1,9 @@
 function selected=rws(fvals,Points)
 
+csum=cumsum(fvals);
 for j=1:length(Points)
     i=1;
-    while sum(fvals(1:i))<Points(j)
+    while csum(i)<Points(j)
         i=i+1;
     end;
     selected(j)=i;
