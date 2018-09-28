ca6c55fbc6b3a501793753040a61950ba53f35d5
Bence Acs <acs.bence94@gmail.com>
2018-09-28 18:05:05 +0200

    AB: Upgraded mysga

--- matlab/mysga.m
+++ matlab/mysga.m
@@ -4,6 +4,8 @@ population=initpop;
 for i=1:maxgen
     objvals=feval(objfun,population);
     [objvals,index]=sort(objvals);
+    err(i)=objvals(1);
+    avg_err(i)=mean(objvals);
     population=population(index,:);
     fvals=fitness(objvals);
     selected=select(fvals,floor(size(population,1)*ggap));
@@ -13,7 +15,31 @@ for i=1:maxgen
     offspring=mutate(offspring,mutpr);
     offspring=offspring(index,:);
     population=reinsert(population,offspring,floor(size(population,1)*(1-rrate)));
+    fprintf('Finished generation %d\n',i);
 end;
+objvals=feval(objfun,population);
+[objvals,index]=sort(objvals);
+population=population(index,:);
 fpop=population;
+err(maxgen+1)=objvals(1);
 
-
+if strcmp(objfun,'pid_fctn')
+    best=population(1,:);
+    Ap=best(1);
+    Td=best(2);
+    Ti=best(3);
+    Tc=Td*0.1;
+    [t,x,y,r,u]=sim('gentank',[],simset('SrcWorkspace','current'));
+    figure(2);
+    plot(t,y);
+    title('Az algoritmus végén legjobbnak ítélt szabályzó ugrásválasza');
+    figure(3);
+    plot(err);
+    title('A generációnkénti legjobb költségfüggvény érték');
+    figure(4);
+    plot(avg_err);
+    title('A generációnkénti átlag költségfüggvény érték');
+    figure(5);
+    plot(t,u);
+    title('Az algoritmus végén legjobbnak ítélt szabályzó beavatkozójele');
+end;
